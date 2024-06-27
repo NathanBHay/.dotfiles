d5aeabcf5e7752a3d3e0615565abbcda32b5daa9
Nathan Hay <72686983+NathanBHay@users.noreply.github.com>
2024-06-27 21:06:19 +1000

    Switched OS to Nixos
    Switched from ARCH to Nixos.
    Added Nixos config files for home-manager and flakes.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nathan's Nixos Config Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
